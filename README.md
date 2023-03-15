bfcbb4839d23e9821ecbc850db3cc1068dc0f8ff
IGDERZEN-Zakaria <zakaria.igderzen@gmail.com>
2023-03-15 15:10:34 +0100

    Simulate Consumer with kafka Script

--- README.md
+++ README.md
@@ -11,3 +11,14 @@ https://kafka.apache.org/quickstart
 
 ![pic02.png](images%2Fpic02.png)
 
+### Reading events with Kafka script 
+
+get inside Kafka directory and execute this commande
+
+```
+bin/kafka-console-consumer.sh --topic "Topic_Name" --from-beginning --bootstrap-server localhost:9092
+
+bin/kafka-console-consumer.sh --topic amigoscode --from-beginning --bootstrap-server localhost:9092
+```
+
+
--- src/main/java/com/amigoscode/KafkaApplication.java
+++ src/main/java/com/amigoscode/KafkaApplication.java
@@ -18,7 +18,9 @@ public class KafkaApplication {
     @Bean
     CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
         return args -> {
-            kafkaTemplate.send("amigoscode", "Hello From Kafka By Zack :D");
+            for(int i=0;i<10_000_000;i++){
+            kafkaTemplate.send("amigoscode", "Hello From Kafka By Zack :D " + i );
+            }
         };
     }
 }
